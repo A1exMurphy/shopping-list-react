06aae7c651ea25e0c39a48d08219645bc92eb172
Evan Langer <elanger@live.com>
2023-11-22 10:35:29 -0600

    created a server and wrote sql commands

--- database.sql
+++ database.sql
@@ -1,2 +1,16 @@
--- Don't forget to add your create table SQL 
--- It is also helpful to include some test data
+CREATE TABLE shopping_list (
+    "id" SERIAL PRIMARY KEY,
+    "Name" VARCHAR(80) NOT NULL,
+    "Quantity" DECIMAL,
+    "Unit" VARCHAR(20)
+);
+
+INSERT INTO shopping_list ("Name", "Quantity", "Unit")
+    VALUES ('cumquats', 12, 'delicious');
+
+DELETE FROM shopping_list 
+    WHERE "id" = ($1)
+
+UPDATE "shopping_list"
+	SET "Unit" = 'Super delicious'
+	WHERE "id" = ($1);
