c2742ac4b3e9ab0e345c93a2799ecd077b264cb4
Evan Langer <elanger@live.com>
2023-11-22 11:39:02 -0600

    made POST in router

--- server/server.router.js
+++ server/server.router.js
@@ -7,12 +7,14 @@ const pool = require('./modules/pool.js');
 
 //GET route
 router.get('/', (req, res) => {
+    console.log('in GET route')
     let queryText = 
         `
         SELECT * FROM "shopping_list"
             ORDER BY "id";
         `
-   .then((dbresult) => {
+        pool.query(queryText)
+            .then((dbresult) => {
         console.log(dbresult.rows, 'this is the db result.rows');
         res.send(dbresult.rows);
     }).catch(dbError => {
@@ -23,7 +25,26 @@ router.get('/', (req, res) => {
 
 
 //POST route
+router.post('/', (req, res) => {
+    console.log('in POST route')
+    let queryText = 
+        `
+        INSERT INTO shopping_list ("Name", "Quantity", "Unit")
+            VALUES ($1, $2,
+        `
+        console.log(req.body, `req.body`)
+        const sqlValues = [
+            req.body//.newItem
+        ]
 
+            pool.query(queryText, sqlValues)
+            .then((dbResult) => {
+                res.sendStatus(201)
+            })
+            .catch((dbError) => {
+                console.log(dbError, `error POST query to db`)
+            })
+})
 
 
 //PUT route
