96685100462dfadaee0ee2274883575972c47043
Evan Langer <elanger@live.com>
2023-11-22 10:45:45 -0600

    created router file and imported to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const shoppingList = require('./server.router.js');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
---
+++ server/server.router.js
@@ -0,0 +1,3 @@
+const router = require('express').Router();
+const pool = require('./modules/pool.js');
+
